47eb6d2b0a9baa6ffdec9481a1802bbc283a3550
Ferrero Cristian <cristian.ferrero1992@gmail.com>
2019-09-13 20:45:24 -0300

    Parallax implementation

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+const FIRST_PARALAX_MIN_HEIGHT = 230;
+
 var masterViewModel = 
 {
     test1 : ko.observable("TEST")
@@ -5,38 +7,48 @@ var masterViewModel =
 
 //Comportamiento de la imagen de perfil en el scroll.
 var profilePicScaleOnScroll = () =>{
-	let profilePicElement = document.getElementById("cbf-profile-pic");
+	let profilePicElement = document.getElementById("cbf-section-1");
 	let rect;
-	let viewPortHeight = $(window).height();
-	let halfViewPortHight = viewPortHeight / 2;
-	let closeless = viewPortHeight * .3;
-
-	console.log(viewPortHeight, halfViewPortHight, closeless);
-
+	let viewPortHeight = parseInt($(window).height());
+	let halfViewPortHight = parseInt(viewPortHeight / 2);
+	let closeless = parseInt(viewPortHeight * .5);
+	
+	// console.log(viewPortHeight, halfViewPortHight, closeless);
+	
 	$(document).scroll(()=>{
 		rect = profilePicElement.getBoundingClientRect();
 		// console.log(rect.top, rect.bottom);
+		let imagePosition = parseInt((rect.bottom+rect.top)/2);
 		
-		if( halfViewPortHight-rect.top <= closeless && halfViewPortHight-rect.top>=0 )
+		if( imagePosition>0 && Math.abs(parseInt(halfViewPortHight-imagePosition))<= closeless )
 		{
-			console.log("a");
-			(!$("#cbf-profile-pic").hasClass("scaled")) ? $(profilePicElement).addClass("scaled") : undefined;
+			$("#cbf-section-1").hasClass("hidden") ? $(profilePicElement).removeClass("hidden") : undefined;
 		}
 		else
 		{
-			console.log("b");
-			$("#cbf-profile-pic").hasClass("scaled") ? $(profilePicElement).removeClass("scaled") : undefined;
+			(!$("#cbf-section-1").hasClass("hidden")) ? $(profilePicElement).addClass("hidden") : undefined;
 		}
 	});
 };
+
+var setFirstParallaxHeight = () =>{
+	let viewPortHeight = parseInt($(window).height());
+	let firstSectionHeight = parseInt($("#cbf-section-1").height());
+	let firstParallaxHeight = viewPortHeight - firstSectionHeight;
+	
+	firstParallaxHeight = firstParallaxHeight<FIRST_PARALAX_MIN_HEIGHT ? FIRST_PARALAX_MIN_HEIGHT : firstParallaxHeight;
+
+	$("#cbf-parallax-1").css("height", firstParallaxHeight.toString());
+}
 
 $(document).ready(function()
 {
 	ko.applyBindings( masterViewModel );
 	$('.parallax').parallax();
+	$('.tooltipped').tooltip();
 
 
-	
+	setFirstParallaxHeight();
 
 	profilePicScaleOnScroll();
 	
