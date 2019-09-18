7b7d143acff1e1f162c7e1abd80dc039ac648cfd
Ferrero Cristian <cristian.ferrero1992@gmail.com>
2019-09-17 21:16:09 -0300

    Agregada experiencia laboral

--- js/index.js
+++ js/index.js
@@ -7,45 +7,46 @@ var masterViewModel =
 
 //Comportamiento de la imagen de perfil en el scroll.
 var profilePicScaleOnScroll = () =>{
-	let profilePicElement = document.getElementById("cbf-section-1");
-	let rect;
-	let viewPortHeight = parseInt($(window).height());
-	let halfViewPortHight = parseInt(viewPortHeight / 2);
-	let closeless = parseInt(viewPortHeight * .5);
-	
-	// console.log(viewPortHeight, halfViewPortHight, closeless);
-	
-	$(document).scroll(()=>{
-		rect = profilePicElement.getBoundingClientRect();
-		// console.log(rect.top, rect.bottom);
-		let imagePosition = parseInt((rect.bottom+rect.top)/2);
+	let animatedElementsIds = ["cbf-section-1", "cbf-section-2"];
+
+	animatedElementsIds.forEach( animatedElementId =>{
+		let animatedHtmlElement = document.getElementById(animatedElementId);
+		let rect;
+		let viewPortHeight = parseInt($(window).height());
+		let halfViewPortHight = parseInt(viewPortHeight / 2);
+		let closeless = parseInt(viewPortHeight * .35);
 		
-		if( imagePosition>0 && Math.abs(parseInt(halfViewPortHight-imagePosition))<= closeless )
-		{
-			$("#cbf-section-1").hasClass("hidden") ? $(profilePicElement).removeClass("hidden") : undefined;
-		}
-		else
-		{
-			(!$("#cbf-section-1").hasClass("hidden")) ? $(profilePicElement).addClass("hidden") : undefined;
-		}
+		$(document).scroll(()=>{
+			rect = animatedHtmlElement.getBoundingClientRect();
+			// console.log(rect.top, rect.bottom);
+			let imagePosition = parseInt((rect.bottom+rect.top)/2);
+			
+			if( imagePosition>0 && Math.abs(parseInt(halfViewPortHight-imagePosition))<= closeless )
+			{
+				$(`#${animatedElementId}`).hasClass("hidden") ? $(animatedHtmlElement).removeClass("hidden") : undefined;
+			}
+			else
+			{
+				(!$(`#${animatedElementId}`).hasClass("hidden")) ? $(animatedHtmlElement).addClass("hidden") : undefined;
+			}
+		});
 	});
 };
 
 var setFirstParallaxHeight = () =>{
-	let viewPortHeight = parseInt($(window).height());
-	let firstSectionHeight = parseInt($("#cbf-section-1").height());
-	let firstParallaxHeight = viewPortHeight - firstSectionHeight;
+	// let viewPortHeight = parseInt($(window).height());
+	// let firstSectionHeight = parseInt($("#cbf-section-1").height());
+	// let firstParallaxHeight = viewPortHeight - firstSectionHeight;
 	
-	firstParallaxHeight = firstParallaxHeight<FIRST_PARALAX_MIN_HEIGHT ? FIRST_PARALAX_MIN_HEIGHT : firstParallaxHeight;
+	// firstParallaxHeight = firstParallaxHeight<FIRST_PARALAX_MIN_HEIGHT ? FIRST_PARALAX_MIN_HEIGHT : firstParallaxHeight;
 
-	$("#cbf-parallax-1").css("height", firstParallaxHeight.toString());
+	// $("#cbf-parallax-1").css("height", firstParallaxHeight.toString());
 }
 
 $(document).ready(function()
 {
 	ko.applyBindings( masterViewModel );
-	$('.parallax').parallax();
-	$('.tooltipped').tooltip();
+	M.AutoInit();
 
 
 	setFirstParallaxHeight();
